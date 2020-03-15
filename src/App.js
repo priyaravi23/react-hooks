bba8dbe388f3371a2c343c1b6777f68c453724a5
Priya Ravi <priyaravi23@gmail.com>
2020-03-15 13:00:29 -0700

    making api calls with react hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+    const [comments, setComments] = useState([]);
 
-export default App;
+    useEffect(() => {
+        axios("https://jsonplaceholder.typicode.com/comments")
+            .then(response =>
+                response.data.map(comment => ({
+                    name: `${comment.name}`,
+                    email: `${comment.email}`
+                }))
+            )
+            .then(data => {
+                setComments(data);
+            });
+    }, []);
+
+    return (
+        <div className="users">
+            {comments.map(comment => (
+                <div key={comment.name} className="users__user">
+                    <div className="users__meta">
+                        <h1>{comment.name}</h1>
+                        <p>{comment.email}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
